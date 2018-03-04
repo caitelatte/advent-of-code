c54ed57fe8f8d956d1481069dc834221f0241c00
Caitlin <caitelatte@gmail.com>
2018-03-04 21:11:46 +1100

    Days 8 and 9 2017

---
+++ 2017-python/d08-solution.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""Advent of Code 2017 Day 8 - I Heard You Like Registers
+
+Given a set of instructions to conditionally increment or decrement registers, return the largest value in any register after following each.
+"""
+
+INPUT_FILENAME = "d08-input.txt"
+
+def parse_instruction(instruct):
+    """Given a line of instruction, return a dictionary of how to use it.
+    """
+    spl_instruct = instruct.split()
+    if spl_instruct[1] == "inc":
+        increment = int(spl_instruct[2])
+    else:
+        increment = -int(spl_instruct[2])
+    return {
+        "target_name": spl_instruct[0],
+        "target_inc": increment,
+        "cond_name": spl_instruct[4],
+        "cond_bool": spl_instruct[5],
+        "cond_val": int(spl_instruct[6])
+    }
+
+
+def process_instruction(instruct, registers):
+    """Given an instruction and a set of registers, update the register."""
+    global max_val
+    p_ins = parse_instruction(instruct)
+    # calculate the conditional
+    cond_true = False
+    cond_reg = registers.get(p_ins["cond_name"], 0)
+    if p_ins["cond_bool"] == "==":
+        cond_true = (cond_reg == p_ins["cond_val"])
+    elif p_ins["cond_bool"] == "!=":
+        cond_true = (cond_reg != p_ins["cond_val"])
+    elif p_ins["cond_bool"] == ">":
+        cond_true = (cond_reg > p_ins["cond_val"])
+    elif p_ins["cond_bool"] == ">=":
+        cond_true = (cond_reg >= p_ins["cond_val"])
+    elif p_ins["cond_bool"] == "<":
+        cond_true = (cond_reg < p_ins["cond_val"])
+    elif p_ins["cond_bool"] == "<=":
+        cond_true = (cond_reg <= p_ins["cond_val"])
+
+    if cond_true:
+        # update register if conditional true
+        new_val = registers.get(p_ins["target_name"], 0) + p_ins["target_inc"]
+        registers[p_ins["target_name"]] = new_val
+        if new_val > max_val:
+            max_val = new_val
+        print "{result}: ({comp_name}) {comp_value} {cond_bool} {cond_val}, therefore incrementing {target_name} by {target_inc}".format(
+            result=cond_true,
+            comp_name=p_ins["cond_name"],
+            comp_value=cond_reg,
+            cond_bool=p_ins["cond_bool"],
+            cond_val=p_ins["cond_val"],
+            target_name=p_ins["target_name"],
+            target_inc=p_ins["target_inc"]
+        )
+
+
+if (__name__ == "__main__"):
+    instructions = []
+    registers = {}
+    global max_val
+    max_val = 0
+    # Take in input from file
+    with (open(INPUT_FILENAME)) as f:
+        for line in f:
+            process_instruction(line.strip(), registers)
+
+    print "The largest value is: " + str(max(registers.values()))
+
+    print "The largest value at any time during the calculation was: " + str(max_val)
---
+++ 2017-python/d09-solution.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+"""Advent of Code 2017 Day 9 - Stream Processing
+
+Given a chunk of text which represents a "group", return its total score.
+
+A "group" is consisted of {}
+"""
+
+INPUT_FILENAME = "d09-input.txt"
+
+def process_char(char, state):
+    """given a character and a state, returns a tuple of result, state
+
+    result may be:
+        - "new_group"
+        - "end_group"
+        - "garbage_char"
+        - None
+
+    state may be:
+        - "garbage"
+        - "garbage_escaped"
+        - None
+    """
+    result = None
+    # if escaping a character, nothing matters but changing out of escaped
+    if state == "garbage_escaped":
+        state = "garbage"
+    elif state == "garbage":
+        if char == ">":
+            state = None
+        elif char == "!":
+            state = "garbage_escaped"
+        else:
+            result = "garbage_char"
+    elif char == "<":
+        state = "garbage"
+    elif char == "{":
+        result = "new_group"
+    elif char == "}":
+        result = "end_group"
+    elif char != ",":
+        print "didn't recognise char " + char
+    return result, state
+
+if (__name__ == "__main__"):
+    # # Test on simple input
+    # in_str = "{{<!!>},{<!!>},{<!!>},{<!!>}}"
+
+    # Take in input from file
+    with (open(INPUT_FILENAME)) as f:
+        in_str = f.read().strip()
+
+    # Process stream
+    state = None
+    depth = 0
+    score = 0
+    garbage_chars = 0
+    clean_groups = ""
+    for char in in_str:
+        char_result, state = process_char(char, state)
+        if char_result == "new_group":
+            depth += 1
+            clean_groups += "["
+        elif char_result == "end_group":
+            score += depth
+            clean_groups = clean_groups.strip(",")
+            clean_groups += "],"
+            depth -= 1
+        elif char_result == "garbage_char":
+            garbage_chars += 1
+
+    print clean_groups
+
+    print "End depth is " + str(depth)
+    print "End score is {0}".format(score)
+    print "Number of non-escaped garbage chars is {0}".format(garbage_chars)
